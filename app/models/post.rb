349c605bb93b57106cda0f6d67d483de8b407175
Alena Trushnikova <alena.yu.t@gmail.com>
2020-12-16 15:45:59 -0800

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,12 @@ class Post < ActiveRecord::Base
 
   belongs_to :author
   validate :is_title_case 
+  # before_save :make_title_case
+  before_validation :make_title_case
+
+  # Here is a rule of thumb: Whenever you are modifying an attribute of 
+  # the model, use before_validation. 
+  # If you are doing some other action, then use before_save.
 
   private
 
